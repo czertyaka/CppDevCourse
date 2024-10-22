def67013f81e9caa9181f2ccffa5478d1618c5f0
czertyaka <pyhovoleg31@gmail.com>
2024-10-22 19:03:26 +0500

    updated 3rd presentation

---
+++ Presentations/3-Fundamental-types/char.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    char a = 65;
+    std::cout << a << std::endl;
+    char b = 'b';
+    std::cout << b << std::endl;
+    std::cout << "this is tab" << '\t'
+        << "and this '\n' is newline"
+        << std::endl;
+}
